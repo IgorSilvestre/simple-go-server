ec78fee12136422432434206d27eadbea7e55852
igor Silvestre <igormorais.silvestre@icloud.com>
2024-07-15 10:26:12 -0300

    refactor: cors

--- main.go
+++ main.go
@@ -9,6 +9,19 @@ import (
     "github.com/rs/cors"
 )
 
+// CORS Middleware
+func corsMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "https://gtrinvestimentos-frontend.vercel.app")
+        if r.Method == "OPTIONS" {
+            w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+            w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+            return
+        }
+        next.ServeHTTP(w, r)
+    })
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello World!")
 }
@@ -41,18 +54,9 @@ func main () {
     r.HandleFunc("/", handler)
 
 
-    // Configure CORS
-    c := cors.New(cors.Options{
-        AllowedOrigins:   []string{"https://gtrinvestimentos-frontend.vercel.app"}, // Allow your frontend origin
-        AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
-        AllowedHeaders:   []string{"Authorization", "Content-Type"},
-        AllowCredentials: true,
-    })
+    r.Use(corsMiddleware)
 
-    // Use the CORS middleware
-    handler := c.Handler(r)
-
-    if err := http.ListenAndServe(":8080", handler); err != nil {
+    if err := http.ListenAndServe(":8080", r); err != nil {
         log.Fatalf("Failed to start server: %v", err)
     } else {
         log.Println("Server started on port 8080")
